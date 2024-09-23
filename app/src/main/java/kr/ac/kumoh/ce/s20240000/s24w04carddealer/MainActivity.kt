3ebe6150f95e8d6bb38496fc52d4a624567b60a0
BWOH <bwoh@outlook.kr>
2024-09-24 00:43:47 +0900

    🎉 Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20240000/s24w04carddealer/MainActivity.kt
@@ -0,0 +1,28 @@
+package kr.ac.kumoh.ce.s20240000.s24w04carddealer
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kr.ac.kumoh.ce.s20240000.s24w04carddealer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var main: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        //setContentView(R.layout.activity_main)
+        main= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(main.root)
+
+        main.imgCard1.setImageResource(R.drawable.c_2_of_hearts)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
